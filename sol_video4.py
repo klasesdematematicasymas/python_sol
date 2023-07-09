243415cc14b581395223931ee970eb4653d92389
klasesdematematicasymas <139070423+klasesdematematicasymas@users.noreply.github.com>
2023-07-09 12:48:22 -0500

    Add files via upload

---
+++ sol_video4.py
@@ -0,0 +1,6 @@
+x=float(input("Digite el primer número: "))
+y=float(input("Digite el segundo número: "))
+print("La suma es: ", x+y)
+print("La resta es: ", x-y)
+print("La multiplicación es: ",x*y)
+print("La división es: ",x/y)
