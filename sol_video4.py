f9ec44d2af28dee193e8c903d855851caffd0658
klasesdematematicasymas <139070423+klasesdematematicasymas@users.noreply.github.com>
2023-07-09 12:50:39 -0500

    Update sol_video4.py

--- sol_video4.py
+++ sol_video4.py
@@ -1,5 +1,7 @@
+#input lee palabras y se debe transformar a números reales
 x=float(input("Digite el primer número: "))
 y=float(input("Digite el segundo número: "))
+#print puede imprimir palabras y números si se separan con comas
 print("La suma es: ", x+y)
 print("La resta es: ", x-y)
 print("La multiplicación es: ",x*y)
